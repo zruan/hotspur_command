522c66c2485a0592e778bf0ac3571877c5652427
Drew Gingerich <gingerich93@gmail.com>
2019-02-28 14:56:00 -0800

    Update collection processor to create symlink, and other changes pulled from my old edits.

--- hotspur_setup.py
+++ hotspur_setup.py
@@ -3,3 +3,4 @@ import os
 base_path = "/pncc/storage/1/processing/hotspur"
 config_dir = os.path.join(base_path, "configs")
 hashlinks_dir = os.path.join(base_path, "hashlinks")
+hash_salt = "feilivinglab-17"
