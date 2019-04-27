116991bd4545d557cab54f82f59874fbf614fecb
DrewGingerich <gingerid@ohsu.edu>
2019-04-27 16:23:31 -0700

    Add fields for available gpus and cpus to hotspur setup file

--- hotspur_setup.py
+++ hotspur_setup.py
@@ -5,3 +5,5 @@ config_dir = os.path.join(base_path, "configs")
 hashlinks_dir = os.path.join(base_path, "hashlinks")
 hash_salt = "feilivinglab-17"
 couchdb_address = "http://localhost:5984/"
+available_gpus = [0, 1, 2, 3]
+available_cpus = 12
