7471c357337235b0091ced016dc6423162b84296
DrewGingerich <gingerid@ohsu.edu>
2019-04-27 16:45:48 -0700

    Create motion correction data class

---
+++ data_models/motion_correction_data.py
@@ -0,0 +1,17 @@
+from data_models import DataModel
+
+
+class MotionCorrectionData(DataModel):
+
+	def __init__(self, base_name):
+		super().__init__(base_name)
+
+		self.aligned_image_file = None
+		self.dose_weighted_image_file = None
+
+		self.shift_list = None
+		self.initial_shift = None
+		self.total_shift = None
+
+		self.pixel_size = None
+		self.dimensions = None
