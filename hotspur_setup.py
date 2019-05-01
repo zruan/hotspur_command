c50bcd3a114bcfe9fa1c6e54a6e62b85f70a9398
DrewGingerich <gingerid@ohsu.edu>
2019-04-30 18:00:22 -0700

    Add username and pass to couchdb address. Remove unused entries in hotspur setup.

--- hotspur_setup.py
+++ hotspur_setup.py
@@ -1,13 +1,12 @@
-import os
-
 base_path = "/pncc/storage/1/processing/hotspur"
 search_glob = [
-	# '/pncc/storage/1/rawdata/pncc/*/*/*/frames/',
-	'/pncc/storage/1/rawdata/pncc-testing/SEM_scripts/test_data_2/'
+	'/pncc/storage/1/rawdata/pncc/*/*/*/frames/'
+	# '/pncc/storage/1/rawdata/pncc/*/*/*/'
+	# '/pncc/storage/1/rawdata/pncc-testing/SEM_scripts/test_data_2/'
 ]
-config_dir = os.path.join(base_path, "configs")
-hashlinks_dir = os.path.join(base_path, "hashlinks")
+
+couchdb_address = "http://pncc:cryoem@localhost:5984/"
 hash_salt = "feilivinglab-17"
-couchdb_address = "http://localhost:5984/"
+
 available_gpus = [0, 1, 2, 3]
 available_cpus = 12
