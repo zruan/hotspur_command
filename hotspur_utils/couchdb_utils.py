4be7c423e21eb813eaa3234491c4729158cf6f3f
DrewGingerich <gingerid@ohsu.edu>
2019-07-18 12:30:09 -0700

    Update couchdb util functions to take sessions

--- hotspur_utils/couchdb_utils.py
+++ hotspur_utils/couchdb_utils.py
@@ -2,25 +2,25 @@ import couchdb
 
 import hotspur_setup
 
-def update_session_list(project_hash, session_hash, session_name):
+def update_session_list(session):
 	try:
-		project_db = get_db(project_hash)
+		project_db = get_db(session.project_hash)
 		session_list_doc = get_doc(session_list_doc_name)
-		session_list_doc[session_name] = session_hash
+		session_list_doc[session_name] = session.hash
 		project_db[session_list_doc_name] = doc
-		print('Added {} to session list.'.format(session_name))
+		print('Added {} to session list.'.format(session.name))
 		return True
 	except Exception as e:
 		print(e)
 		raise e
 
 
-def update_project_list(project_hash, project_name):
+def update_project_list(session):
 	try:
 		doc = get_doc(admin_db, project_list_doc_name)
-		doc[project_name] = project_hash
+		doc[project_name] = session.project_hash
 		admin_db[project_list_doc_name] = doc
-		print('Added {} to project list.'.format(project_name))
+		print('Added {} to project list.'.format(session.project_name))
 		return True
 	except Exception as e:
 		print(e)
@@ -35,10 +35,10 @@ def get_db(db_name):
 
 def get_doc(db, doc_name):
 	try:
-		doc = db[doc]
+		doc = db[doc_name]
 	except:
 		doc = {'_id': doc_name}
-		doc = db.save(doc)
+		db[doc_name] = doc
 	return doc
 
 couchdb_server = couchdb.Server(hotspur_setup.couchdb_address)
