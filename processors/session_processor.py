5621ca8133616b2589ac855a732113e015b5e864
DrewGingerich <gingerid@ohsu.edu>
2019-07-18 14:12:40 -0700

    Update session processor logic and logs.

--- processors/session_processor.py
+++ processors/session_processor.py
@@ -8,7 +8,8 @@ from hotspur_utils import couchdb_utils, filesystem_utils
 class SessionProcessor():
 
     def __init__(self):
-        self.tracked_directories = []
+        self.tracked = []
+        self.queued = []
         self.sessions = []
 
     def find_sessions(self, search_globs):
@@ -17,50 +18,54 @@ class SessionProcessor():
             search_matches.extend(glob(search))
 
         new_matches = [
-            match for match in search_matches if match not in self.tracked_directories]
+            match for match in search_matches if match not in self.tracked]
+        print (new_matches)
+        self.tracked.extend(new_matches)
+        self.queued.extend(new_matches)
 
-        for directory in new_matches:
+        for directory in self.queued:
             print('Found potential session at {}'.format(directory))
-            valid = self.validate_session(directory)
-
-            if valid and directory not in self.tracked_directories:
-                print('Session found at {}'.format(directory))
+            if self.validate_session(directory):
+                print("Session is valid")
                 try:
                     session = self.create_new_session(directory)
                     self.sessions.append(session)
-                    self.tracked_directories.append(directory)
+                    self.queued.remove(directory)
                 except:
                     continue
+            else:
+                print("Session is not valid, skipping")
 
         return self.sessions
 
     def validate_session(self, directory):
         mdoc_files = glob('{}/*.mdoc'.format(directory))
         if len(mdoc_files) == 0:
-            print('No mdoc files found. Skipping...')
             return False
         return True
 
     def create_new_session(self, frames_directory):
         try:
             session = filesystem_utils.extract_session_from_path(frames_directory)
-            print("Successfully derived session from path {}".format(frames_directory))
+            print("Created session metadata")
         except Exception as e:
-            print("Failed to create session from path {}".format(frames_directory))
+            print("Failed to create session metadata")
             print(e)
             raise e
 
-        session.db = couchdb_utils.get_db(session.hash)
+        session.db = couchdb_utils.fetch_db(session.hash)
         if not session.fetch(session.db):
-            print('No previous session data found. Generating...')
             try:
                 couchdb_utils.update_project_list(session)
                 couchdb_utils.update_session_list(session)
+                print('Added session to admin lists')
                 session.push(session.db)
-                print('Session data loaded from couchdb!')
+                print('Session added to database')
             except Exception as e:
-                print('Failed to update database for session {}'.format(session.name))
+                print('Failed to add session to database')
                 print(e)
                 raise e
+        else:
+            print('Updated session metadata from database')
 
         return session
