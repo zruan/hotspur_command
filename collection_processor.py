3fc82a9cc28da4e6128776db829fd7748ad1ccb0
DrewGingerich <gingerid@ohsu.edu>
2019-07-22 13:27:27 -0700

    Remove old commented code

--- collection_processor.py
+++ collection_processor.py
@@ -15,11 +15,11 @@ def arguments():
     parser = argparse.ArgumentParser(
         description='Runs data processing live for incoming data'
     )
-    parser.add_argument(
-        '--target-dir',
-        dest='target_dir',
-        help='A directory you want to process. This is for when you only want to process one session.'
-    )
+    # parser.add_argument(
+    #     '--target-dir',
+    #     dest='target_dir',
+    #     help='A directory you want to process. This is for when you only want to process one session.'
+    # )
     parser.add_argument(
         '--reset-all',
         dest='reset_all',
@@ -37,30 +37,6 @@ def arguments():
         help="Reset all processing done for given frames directory."
     )
     return parser.parse_args()
-
-# def reset_session(session):
-#     server = couchdb.Server(hotspur_setup.couchdb_address)
-#     db = SessionProcessor.session_databases[session]
-#     print('Deleting database "{}"...'.format(db.name))
-#     try:
-#         server.delete(db.name)
-#         print('Database deleted.')
-#     except:
-#         print('Database could not be deleted. Skipping...')
-
-# def reset_all():
-#     server = couchdb.Server(hotspur_setup.couchdb_address)
-#     db = server['hashlinks']
-#     for doc_summary in db.view('_all_docs'):
-#         doc = db[doc_summary.id]
-#         db_name = doc['db_name']
-#         print('Deleting database "{}"...'.format(db_name))
-#         try:
-#             server.delete(doc['db_name'])
-#             print('Database deleted.')
-#         except:
-#             print('Database could not be deleted. Skipping...')
-#     server.delete('hashlinks')
 
 def start_processing():
     args = arguments()
@@ -81,10 +57,10 @@ def start_processing():
         couchdb_utils.reset_session(session)
         exit()
 
-    if args.target_dir is not None:
-        search_globs = [args.target_dir]
-    else:
-        search_globs = hotspur_setup.search_globs
+    # if args.target_dir is not None:
+    #     search_globs = [args.target_dir]
+    # else:
+    #     search_globs = hotspur_setup.search_globs
 
     while True:
         for session in session_processor.find_sessions(search_globs):
