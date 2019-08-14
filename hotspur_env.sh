af686e947c58b2b8a06ecd78f5b017aed9582c19
DrewGingerich <gingerid@ohsu.edu>
2019-08-14 15:01:52 -0700

    Move hotspur configuration to environment variables

---
+++ hotspur_env.sh
@@ -0,0 +1,8 @@
+export HOTSPUR_PATH="/pncc/storage/1/processing/hotspur"
+export HOTSPUR_SEARCH_GLOBS="/pncc/storage/1/rawdata/pncc/*/*/*/frames/"
+export HOTSPUR_ADMIN_NAME="pncc"
+export HOTSPUR_ADMIN_PASS="cryoem"
+export HOTSPUR_HASH_SALT="feilivinglab-17"
+export HOTSPUR_GPUS="0,1,2,3"
+export HOTSPUR_THREADS="12"
+export HOTSPUR_SESSION_MAX_AGE="7"
--- hotspur_setup.py
+++ hotspur_setup.py
@@ -1,14 +1,17 @@
+import os
+import sys
+
 # directory hotspur uses for all its functionality
 base_path = "/pncc/storage/1/processing/hotspur"
 
 search_globs = [
-	# '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/*/*/*/frames/',
-	# '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/50633/20190429-asic-sma/screening-4/frames/',
-	# '/goliath/rawdata/MMC/Confometrx/190618/0426-2_2/frames',
-	'/pncc/storage/1/rawdata/pncc/*/*/*/frames/',
-	# '/pncc/storage/1/rawdata/pncc/*/*/*/screen/',
-	# '/pncc/storage/1/rawdata/pncc/*/*/*/',
-	# '/pncc/storage/1/rawdata/pncc-testing/SEM_scripts/test_data_2/'
+    # '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/*/*/*/frames/',
+    # '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/50633/20190429-asic-sma/screening-4/frames/',
+    # '/goliath/rawdata/MMC/Confometrx/190618/0426-2_2/frames',
+    '/pncc/storage/1/rawdata/pncc/*/*/*/frames/',
+    # '/pncc/storage/1/rawdata/pncc/*/*/*/screen/',
+    # '/pncc/storage/1/rawdata/pncc/*/*/*/',
+    # '/pncc/storage/1/rawdata/pncc-testing/SEM_scripts/test_data_2/'
 ]
 
 couchdb_address = "http://pncc:cryoem@localhost/couchdb/"
@@ -21,3 +24,47 @@ available_cpus = 12
 # If mod time of directory is older, it will be skipped.
 # Run `touch /path/to/dir` on the command line to refresh the mod time.
 session_max_age = 7
+
+
+def setup_from_environment():
+    if "HOTSPUR_PATH" in os.environ:
+        base_path = os.environ["HOTSPUR_PATH"]
+    else:
+        print("No value for HOTSPUR_PATH in environment")
+        sys.exit()
+    print("Hotspur base path set to {}".format(base_path))
+
+    if "HOTSPUR_ADMIN_NAME" in os.environ and "HOTSPUR_ADMIN_PASS" in os.environ:
+        couchdb_address = "http://{}:{}@localhost/couchdb/".format(
+            os.environ["HOTSPUR_ADMIN_NAME"],
+            os.environ["HOTSPUR_ADMIN_PASS"]
+        )
+    elif "HOTSPUR_ADMIN_NAME" in os.environ or "HOTSPUR_ADMIN_PASS" in os.environ:
+        print("Must give both HOTSPUR_ADMIN_NAME and HOTSPUR_ADMIN_PASS")
+        sys.exit()
+    else:
+        print("Must give both HOTSPUR_ADMIN_NAME and HOTSPUR_ADMIN_PASS")
+        sys.exit()
+    print("Received couchdb admin name and pass")
+
+    if "HOTSPUR_SEARCH_GLOBS" in os.environ:
+        search_globs = os.environ["HOTSPUR_SEARCH_GLOBS"].split(":")
+    else:
+        search_globs = []
+    print("Search for patterns {}".format(search_globs))
+
+    hash_salt = os.getenv("HOTSPUR_HASH_SALT",
+                            "Please make me more secure")
+
+    if "HOTSPUR_GPUS" in os.environ:
+        available_gpus = [int(id)
+                            for id in os.environ["HOTSPUR_GPUS"].split(',')]
+    else:
+        available_gpus = [0]
+    print("Using gpus {}".format(available_gpus))
+
+    available_cpus = os.getenv('HOTSPUR_THREADS', 2)
+    print("Using {} threads".format(available_cpus))
+
+
+setup_from_environment()
