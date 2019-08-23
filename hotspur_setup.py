d122d013328269a2fe30bbc77a021a7191deca50
DrewGingerich <gingerid@ohsu.edu>
2019-08-23 16:58:47 -0700

    Fix bug due to non-global references

--- hotspur_setup.py
+++ hotspur_setup.py
@@ -1,31 +1,20 @@
 import os
 import sys
 
-# directory hotspur uses for all its functionality
-base_path = "/pncc/storage/1/processing/hotspur"
-
-search_globs = [
-    # '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/*/*/*/frames/',
-    # '/pncc/storage/1/rawdata/pncc-testing/hotspur-tests/50633/20190429-asic-sma/screening-4/frames/',
-    # '/goliath/rawdata/MMC/Confometrx/190618/0426-2_2/frames',
-    '/pncc/storage/1/rawdata/pncc/*/*/*/frames/',
-    # '/pncc/storage/1/rawdata/pncc/*/*/*/screen/',
-    # '/pncc/storage/1/rawdata/pncc/*/*/*/',
-    # '/pncc/storage/1/rawdata/pncc-testing/SEM_scripts/test_data_2/'
-]
-
-couchdb_address = "http://pncc:cryoem@localhost/couchdb/"
-hash_salt = "feilivinglab-17"
-
-available_gpus = [0, 1, 2, 3]
-available_cpus = 12
-
-# Maximum age of directory for valid session, in days.
-# If mod time of directory is older, it will be skipped.
-# Run `touch /path/to/dir` on the command line to refresh the mod time.
+base_path = None
+search_globs = None
+couchdb_address = None
+hash_salt = None
+available_gpus = None
+available_cpus = None
 session_max_age = None
 
 def setup_from_environment():
+    global base_path
+    global search_globs
+    global couchdb_address
+    global hash_salt
+    global available_gpus
     global available_cpus
     global session_max_age
 
