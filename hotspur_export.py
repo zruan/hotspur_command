dfa1c53d8d7a31ea37e4ec0ee52bd3950528e5c3
DrewGingerich <gingerid@ohsu.edu>
2019-08-28 10:49:12 -0700

    Make export symlink image files for better mobility

--- hotspur_export.py
+++ hotspur_export.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 from hotspur_utils import couchdb_utils
 from data_models import SessionData, AcquisitionData, MotionCorrectionData, CtfData
@@ -39,31 +40,60 @@ def export(args):
         out_base = args.out_dir
     else:
         out_base = session.processing_directory
+
     out_base = os.path.abspath(out_base)
-    out_dir = f"{out_base}/hotspur-export"
-    os.makedirs(out_dir, exist_ok=True)
+
+    export_dir_suffix = 'hotspur-export'
+    frame_dir_suffix = f'{export_dir_suffix}/frames'
+    mc_dir_suffix = f'{export_dir_suffix}/motioncor2'
+    ctf_dir_suffix = f'{export_dir_suffix}/ctffind'
+
+    export_dir = f"{out_base}/{export_dir_suffix}"
+    frame_dir = f'{out_base}/{frame_dir_suffix}'
+    mc_dir = f'{out_base}/{mc_dir_suffix}'
+    ctf_dir = f'{out_base}/{ctf_dir_suffix}'
+
+    os.makedirs(export_dir, exist_ok=True)
+    os.makedirs(frame_dir, exist_ok=True)
+    os.makedirs(mc_dir, exist_ok=True)
+    os.makedirs(ctf_dir, exist_ok=True)
+
+    for ad in [mg.acquisition_data for mg in micrographs]:
+        ad.image_link_path = f'{frame_dir_suffix}/{os.path.basename(ad.image_path)}'
+        link_target = f'{out_base}/{ad.image_link_path}'
+        symlink(ad.image_path, link_target, overwrite=True)
 
     star_string = generate_movie_star(micrographs)
-    with open(f'{out_dir}/movies.star', 'w') as fp:
+    with open(f'{export_dir}/movies.star', 'w') as fp:
         fp.write(star_string)
 
-    meta_out_dir = f'{out_dir}/motioncor_meta'
+    for mcd in [mg.motion_correction_data for mg in micrographs]:
+        mcd.image_link_path = f'{mc_dir_suffix}/{os.path.basename(mcd.corrected_image_file)}'
+        link_target = f'{out_base}/{mcd.image_link_path}'
+        symlink(mcd.corrected_image_file, link_target, overwrite=True)
+
     for micrograph in micrographs:
-        os.makedirs(meta_out_dir, exist_ok=True)
         star_string = generate_motion_metadata_star(micrograph)
-        path = f'{meta_out_dir}/{micrograph.motion_correction_data.base_name}.star'
-        with open(path, 'w') as fp:
+        mcd = micrograph.motion_correction_data
+        mcd.metadata_file = f'{mc_dir_suffix}/{mcd.base_name}.star'
+        full_path = f'{out_base}/{mcd.metadata_file}'
+        with open(full_path, 'w') as fp:
             fp.write(star_string)
 
-    star_string = generate_motion_correction_star(micrographs, meta_out_dir)
-    with open(f'{out_dir}/corrected_micrographs.star', 'w') as fp:
+    star_string = generate_motion_correction_star(micrographs)
+    with open(f'{export_dir}/corrected_micrographs.star', 'w') as fp:
         fp.write(star_string)
+
+    for cd in [mg.ctf_data for mg in micrographs]:
+        cd.image_link_path = f'{ctf_dir_suffix}/{os.path.basename(cd.ctf_image_file)}'
+        link_target = f'{out_base}/{cd.image_link_path}'
+        symlink(cd.ctf_image_file, link_target, overwrite=True)
 
     star_string = generate_ctf_star(micrographs)
-    with open(f'{out_dir}/micrographs_ctf.star', 'w') as fp:
+    with open(f'{export_dir}/micrographs_ctf.star', 'w') as fp:
         fp.write(star_string)
 
-    print(f"Exported project to {out_dir}")
+    print(f"Exported project to {export_dir}")
 
 
 def generate_movie_star(micrographs):
@@ -73,7 +103,7 @@ def generate_movie_star(micrographs):
         "_rlnMicrographMovieName",
         ""
     ])
-    micrograph_paths = [micrograph.acquisition_data.image_path for micrograph in micrographs]
+    micrograph_paths = [micrograph.acquisition_data.image_link_path for micrograph in micrographs]
     star_string += '\n'.join(micrograph_paths)
     return star_string
 
@@ -87,7 +117,7 @@ def generate_motion_metadata_star(micrograph):
         f"_rlnImageSizeX                      {mcd.dimensions[0]}",
         f"_rlnImageSizeY                      {mcd.dimensions[1]}",
         f"_rlnImageSizeZ                      {ad.frame_count}",
-        f"_rlnMicrographMovieName             {mcd.corrected_image_file}",
+        f"_rlnMicrographMovieName             {mcd.image_link_path}",
         f"_rlnMicrographBinning               {mcd.binning}",
         f"_rlnMicrographOriginalPixelSize     {ad.pixel_size}",
         f"_rlnMicrographDoseRate              {ad.frame_dose}",
@@ -109,7 +139,7 @@ def generate_motion_metadata_star(micrograph):
     out_string += '\n'.join(shifts)
     return out_string
 
-def generate_motion_correction_star(micrographs, meta_out_dir):
+def generate_motion_correction_star(micrographs):
     out_string = '\n'.join([
         "data_",
         "loop_",
@@ -123,8 +153,8 @@ def generate_motion_correction_star(micrographs, meta_out_dir):
     rows = []
     for mcd in [micrograph.motion_correction_data for micrograph in micrographs]:
         row = ' '.join([
-            f'{mcd.corrected_image_file}',
-            f'{meta_out_dir}/{mcd.base_name}.star',
+            f'{mcd.image_link_path}',
+            f'{mcd.metadata_file}',
             f'{mcd.total_accumulated_distance}',
             f'{mcd.early_accumulated_distance}',
             f'{mcd.late_accumulated_distance}'
@@ -158,8 +188,8 @@ def generate_ctf_star(micrographs):
     rows = []
     for ad, mcd, cd in [(mg.acquisition_data, mg.motion_correction_data, mg.ctf_data) for mg in micrographs]:
         row = ' '.join([
-            f'{mcd.corrected_image_file}',
-            f'{cd.ctf_image_file}',
+            f'{mcd.image_link_path}',
+            f'{cd.image_link_path}',
             f'{cd.defocus_u}',
             f'{cd.defocus_v}',
             f'astigmatism',
@@ -175,3 +205,42 @@ def generate_ctf_star(micrographs):
         rows.append(row)
     out_string += '\n'.join(rows)
     return out_string
+
+# From https://stackoverflow.com/questions/8299386/modifying-a-symlink-in-python/55742015#55742015
+def symlink(target, link_name, overwrite=False):
+    '''
+    Create a symbolic link named link_name pointing to target.
+    If link_name exists then FileExistsError is raised, unless overwrite=True.
+    When trying to overwrite a directory, IsADirectoryError is raised.
+    '''
+
+    if not overwrite:
+        os.symlink(target, linkname)
+        return
+
+    # os.replace() may fail if files are on different filesystems
+    link_dir = os.path.dirname(link_name)
+
+    # Create link to target with temporary filename
+    while True:
+        temp_link_name = tempfile.mktemp(dir=link_dir)
+
+        # os.* functions mimic as closely as possible system functions
+        # The POSIX symlink() returns EEXIST if link_name already exists
+        # https://pubs.opengroup.org/onlinepubs/9699919799/functions/symlink.html
+        try:
+            os.symlink(target, temp_link_name)
+            break
+        except FileExistsError:
+            pass
+
+    # Replace link_name with temp_link_name
+    try:
+        # Pre-empt os.replace on a directory with a nicer message
+        if os.path.isdir(link_name):
+            raise IsADirectoryError(f"Cannot symlink over existing directory: '{link_name}'")
+        os.replace(temp_link_name, link_name)
+    except:
+        if os.path.islink(temp_link_name):
+            os.remove(temp_link_name)
+        raise
