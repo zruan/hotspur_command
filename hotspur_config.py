27cd55bc778b1c37f4d5f42724ef38828b2ed546
DrewGingerich <gingerid@ohsu.edu>
2019-10-03 09:28:09 -0700

    Add comments to default yaml config file

---
+++ hotspur_config.py
@@ -0,0 +1,11 @@
+import yaml
+from pathlib import Path
+
+def get_default_config():
+    Path
+
+def load_config(path):
+    config = yaml.load(path)
+
+def validate_config(config):
+    return True
