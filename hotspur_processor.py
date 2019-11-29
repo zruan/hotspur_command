369851b3801013a580d3b74febe5961d7ea2b51d
DrewGingerich <gingerid@ohsu.edu>
2019-11-29 14:42:17 -0800

    Update main loop to include nav and montage processors

--- hotspur_processor.py
+++ hotspur_processor.py
@@ -1,26 +1,45 @@
 import time
+import random
 
-from hotspur_utils import couchdb_utils
+from hotspur_utils.logging_utils import get_logger_for_module
 
-from processors import SessionProcessor, FramesFileProcessor, Motioncor2Processor, CtffindProcessor
+from processors import (
+    SessionProcessor,
+    ProjectProcessor,
+    NavigatorProcessor,
+    MontageProcessor,
+    FramesFileProcessor,
+    Motioncor2Processor,
+    CtffindProcessor
+)
+
 from hotspur_config import get_config
 
+
+logger = get_logger_for_module(__name__)
 
 def start_processing(args):
 
     if args.dirs is not None:
-        search_globs = args.dirs
+        search_patterns = args.dirs
     else:
-        search_globs = get_config().globs
+        search_patterns = get_config().search_patterns
+        logger.debug('Using search patterns from config file')
 
     session_processor = SessionProcessor()
+    project_processor = ProjectProcessor()
 
     while True:
+        logger.debug('Starting main loop')
+        session_processor.find_sessions(search_patterns)
+        random.shuffle(session_processor.sessions)
 
-        for session in session_processor.find_sessions(search_globs):
+        for session in session_processor.sessions:
+            project_processor.update_project(session)
+            NavigatorProcessor.for_session(session).run()
+            MontageProcessor.for_session(session).run()
             FramesFileProcessor.for_session(session).run()
             Motioncor2Processor.for_session(session).run()
             CtffindProcessor.for_session(session).run()
-            continue
 
         time.sleep(5)
