70df83d7ad5293673be2383fa01bad6e0b8d0398
elferich <elferich@hamlet.ohsu.edu>
2020-01-27 16:03:53 -0800

    INtroducing a statusbar

--- subcommands/process/main.py
+++ subcommands/process/main.py
@@ -1,8 +1,11 @@
 import time
 import random
+import tqdm
+import time
 
-from utils.logging import get_logger_for_module
+from utils.logging import get_logger_for_module, get_status_bar
 from utils.resources import ResourceManager
+from utils.config import get_config
 
 from processors import (
     SessionProcessor,
@@ -17,33 +20,43 @@ from processors import (
 
 from utils.config import get_config
 
+session_tracking_interval = 300
 
 LOG = get_logger_for_module(__name__)
+
+STATUS_BAR = get_status_bar()
+
 
 def run(args):
 
     search_patterns = get_config().search_patterns
+    tot_cpus = get_config().threads
 
     session_processor = SessionProcessor()
     project_processor = ProjectProcessor()
 
+    last_session_tracking = None
+    last_main_loop = None
+
     while True:
-        LOG.debug('Starting main loop')
-        LOG.debug(f'Processing {len(session_processor.sessions)} sessions')
-        LOG.debug(f'Available CPUs: {ResourceManager.available_cpus} ')
-        LOG.debug(f'Available GPUs: {ResourceManager.gpu_locks} ')
-        session_processor.find_sessions(search_patterns)
+        if last_main_loop is not None:
+            main_loop_time = time.time() - last_main_loop
+        else:
+            main_loop_time = time.time()
+        last_main_loop = time.time()
+        STATUS_BAR.set_description_str(f'CPU: {ResourceManager.available_cpus}/{tot_cpus} GPU: {sum(map(lambda x: not x[1].locked(),ResourceManager.gpu_locks))}/{len(ResourceManager.gpu_locks)} Sessions: {len(session_processor.sessions)} Mainloop time: {main_loop_time}')
+        
+        if last_session_tracking is None or time.time() - last_session_tracking >= session_tracking_interval:
+            session_processor.find_sessions(search_patterns)
+            last_session_tracking = time.time()
+
         random.shuffle(session_processor.sessions)
 
         for session in session_processor.sessions:
-            LOG.info(f'Main loop session {session}')
             project_processor.update_project(session)
             NavigatorProcessor.for_session(session).run()
-            LOG.info(f'Main loop Montage  {session}')
             MontageProcessor.for_session(session).run()
-            LOG.info(f'Main loop Frame Files  {session}')
             FramesFileProcessor.for_session(session).run()
-            LOG.info(f'Main loop Motioncor  {session}')
             Motioncor2Processor.for_session(session).run()
             CtffindProcessor.for_session(session).run()
             DogpickerProcessor.for_session(session).run()
--- utils/logging.py
+++ utils/logging.py
@@ -2,12 +2,27 @@ import logging
 from logging.handlers import RotatingFileHandler
 import sys
 from utils.config import get_config
+import tqdm
+
+class TqdmHandler(logging.StreamHandler):
+    def __init__(self,status_bar):
+        
+        logging.StreamHandler.__init__(self)
+        self.status_bar = status_bar
+
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.status_bar.write(msg)
+
+
+status_bar = tqdm.tqdm(total=0, position=1, bar_format='{desc}')
 
 
 full_formatter = logging.Formatter('\n%(levelname)s - %(asctime)s - %(name)s - %(message)s')
 short_formatter = logging.Formatter('\n%(levelname)s - %(name)s - %(message)s')
 
-sysout_handler = logging.StreamHandler(sys.stdout)
+sysout_handler = TqdmHandler(status_bar)
 sysout_handler.setFormatter(short_formatter)                                                                                        
 sysout_handler.setLevel(logging.INFO)
 
@@ -29,3 +44,6 @@ def get_logger_for_module(module_name):
     logger.addHandler(file_handler)
 
     return logger
+
+def get_status_bar():
+    return status_bar
